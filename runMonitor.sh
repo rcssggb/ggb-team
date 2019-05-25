31e9b8a3fda69d143475eb9715eed2e623d50042
Bruno Andreghetti Dantas <brunoandreghetti@gmail.com>
2019-05-25 12:27:17 -0300

    Add runWithMonitor.sh script to setup X server before running monitor

--- runMonitor.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-HOST_OS=$(uname -s)
-if [ $HOST_OS == "Linux" ]; then
-    echo "linux"
-elif [ $HOST_OS == "Darwin" ]; then
-    echo "mac"
-fi
---
+++ runWithMonitor.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+HOST_OS=$(uname -s)
+if [ $HOST_OS == "Linux" ]; then
+    export DISPLAY=${DISPLAY}
+    xhost +local:root
+elif [ $HOST_OS == "Darwin" ]; then
+    export DISPLAY="host.docker.internal:0"
+    xhost + 127.0.0.1
+else
+    echo "unknown host os"
+    exit
+fi
+
+docker-compose up
+
+if [ $HOST_OS == "Linux" ]; then
+    xhost -local:root
+elif [ $HOST_OS == "Darwin" ]; then
+    export DISPLAY="host.docker.internal:0"
+    xhost - 127.0.0.1
+fi
