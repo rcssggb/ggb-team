ffec672426cd809d61c98d7261f0d6010e01182a
Samuel Venzi <samuel.venzi@me.com>
2019-05-29 20:10:42 -0300

    Remove rcssparser

--- team/rcssparser/split.go
+++
@@ -1,9 +0,0 @@
-package rcssparser
-
-import "strings"
-
-func breakArgs(argStr string) []string {
-	argStr = strings.TrimPrefix(argStr, "(")
-	argStr = strings.TrimSuffix(argStr, ")")
-	return strings.Split(argStr, " ")
-}
