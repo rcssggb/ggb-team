4e0449437a56870ee46ff1ffb612d90a7363f453
Samuel Venzi <samuel.venzi@me.com>
2019-08-30 17:41:11 -0300

    Change message trim solution

--- team/playerclient/Listen.go
+++ team/playerclient/Listen.go
@@ -1,7 +1,6 @@
 package playerclient
 
 import (
-	"bytes"
 	"log"
 	"time"
 )
@@ -11,18 +10,14 @@ func (p *Player) Listen() {
 	response := make([]byte, 8192)
 
 	for {
-		// Clear slice while keeping memory allocated so that it prevents old messages from showing up at the end
-		for i := range response {
-			response[i] = 0
-		}
 		p.conn.SetReadDeadline(time.Now().Add(1 * time.Second))
-		_, _, err := p.conn.ReadFromUDP(response)
+		n, _, err := p.conn.ReadFromUDP(response)
 		now := time.Now()
 		if err != nil {
 			log.Println(err)
 			continue
 		}
-		data := bytes.Trim(response, "\x00")
+		data := response[:n]
 		p.cmdChannel <- Message{
 			timestamp: now,
 			data:      string(data),
